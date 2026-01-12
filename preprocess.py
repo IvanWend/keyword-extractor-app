26f68adb0dc24511066512b016f91fd5ba54de11
IvanWend <forzz9@gmail.com>
2026-01-12 22:50:38 +0800

    Improved the performance

--- preprocess.py
+++ preprocess.py
@@ -6,16 +6,12 @@ nltk.download("stopwords")
 
 stopwords_en = set(stopwords.words("english"))
 stopwords_ru = set(stopwords.words("russian"))
+STOPWORDS = stopwords_en | stopwords_ru
 
 def preprocess(text):
 
     text = text.lower()
     text = re.sub(r"[^a-zа-яё\s]", "", text)
     tokens = text.split()
-    tokens = [
-        t for t in tokens 
-        if t not in stopwords_en and t not in stopwords_ru
-    ]
+    tokens = [t for t in tokens if t not in STOPWORDS]
     return tokens
-
-print(stopwords_ru)
